0a948e740e6a48b24960415c602d814ec303d890
Wes Benvindo <wes.bnvnd@gmail.com>
2025-11-08 10:41:10 +0000

    starting the project

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import {Outlet, Route, Routes} from "react-router";
+
+function App() {
+    return (
+        <Routes>
+            <Route path="" element={<h1>Login</h1>}/>
+
+            <Route path="paciente" element={ <Outlet/>}>
+                <Route index element={<h1>Paciente</h1>}/>
+                <Route path="consultas" element={<h1>Paciente consultas</h1>}/>
+                <Route path="pronturarios" element={<h1>Paciente prontuários</h1>}/>
+            </Route>
+
+            <Route path="profissionail"  element={ <Outlet/>}>
+                <Route index element={<h1>Profissional</h1>}/>
+                <Route path="consultas" element={<h1>Profissional consultas</h1>} />
+                <Route path="pronturarios" element={<h1>Profissional prontuários</h1>} />
+            </Route>
+
+            <Route path="administrador" element={ <Outlet/>}>
+                <Route index element={<h1>Administrador</h1>}/>
+                <Route path="consultas" element={<h1>Administrador consultas</h1>} />
+                <Route path="pronturarios" element={<h1>Administrador prontuários</h1>} />
+            </Route>
+        </Routes>
+    )
+}
+
+export default App
